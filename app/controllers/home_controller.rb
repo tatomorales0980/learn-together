171d36852949d2815278414940b678c5110e79bb
Hector Morales <tatomorales0980@gmail.com>
2019-04-02 02:41:30 +0000

    user form added

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,30 @@
+class HomeController < ApplicationController
+    before_action :authenticate_user!
+    # send back form to create   
+    def new
+    end
+    
+    # create 
+    def create
+    end
+    
+    # list all 
+    def index
+    end
+    
+    # send back to edit
+    def edit
+    end
+    
+    # update 
+    def update
+    end
+    
+    # delete 
+    def delete
+    end
+   
+   #Show the students related with the class 
+    def show    
+    end
+end
