5d3b5927a600a764cc5cedceeb50d90470746be2
Hyeonmin Park <hpark6@my.waketech.edu>
2019-04-09 22:29:30 +0000

    updated messages index.html and routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'home#index'
+  # root 'home#index'
   
-  resources :messages, only: [:index, :new, :create, :destroy] do
+  resources :messages, only: [:index, :new, :create, :destroy, :show] do
     resources :contents, only: [:index, :new, :create]
   end
   
   root 'welcome#homepage'
   
-  get '/users/:id/matches', to: 'home#matches'
-  
+  get '/users/:id/matches', to: 'home#show', as: :match
   
 end
