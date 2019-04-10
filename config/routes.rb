d9d785401a68f32ccd0c0c440b2f515b7a8a9fcd
Hector Morales <tatomorales0980@gmail.com>
2019-04-10 01:19:35 +0000

    errors updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :posts
   root 'home#index'
   # devise_for :users, :controllers => {:registrations => "my_devise/registrations"}
   devise_for :users, controllers: { sessions: 'devise/sessions' }
-
   
   resources :languages do
     delete 'remove'
   end
-  # devise_for :users
-=======
-  devise_for :users
-  root 'welcome#homepage'
-  
-  get '/users/:id/matches', to: 'home#matches'
-  
-  
->>>>>>> master
 end
